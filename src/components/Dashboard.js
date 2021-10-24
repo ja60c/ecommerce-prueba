d6a210c2da94eeafa760855809b5c235355a643e
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-10-24 01:27:49 -0500

    Creando dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <h1 className="display-3 text-center m-3">Hello from Dashboard</h1>
+    )
+}
+
+export default Dashboard; 
