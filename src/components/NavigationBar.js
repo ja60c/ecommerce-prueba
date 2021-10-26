3a72450284d090aab948b7b4f8a7cb76900fb34e
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-10-26 17:45:44 -0500

    Agregando botón de búsqueda a navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Form, FormControl, Image, Nav, Navbar  } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
@@ -30,24 +30,33 @@ function NavigationBar() {
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
             <Nav.Link as={ Link } to="/">Products</Nav.Link>
-            {
-                currentUser
-                ? <Nav.Link as={ Link } to="/profile">Profile</Nav.Link>
-                : ''
-            }
             </Nav>
             <Nav className="ms-auto">
-            {
+            { 
                 currentUser
-                ? <Navbar.Text className="">Signed in as: {currentUser.email}</Navbar.Text>
+                ? <Navbar.Text className="">Hello, <Link to="/profile">{currentUser.displayName}</Link>!</Navbar.Text>
                 : <Nav.Link as={ Link } to="/login">Login</Nav.Link>
             }
+            {/* {
+                !loading && currentUser.photoURL && <div style={{ width: '50px', height: '50px', margin: '0 auto'}}>
+                <Image style={{ objectFit: 'cover', objectPosition: 'center'}} className="w-100 h-100 border border-2 border-secondary p-1" src={ currentUser.photoURL } roundedCircle />
+                </div>
+            } */}
             {
                 currentUser
                 ? <Nav.Link as={ Link } to="/login" onClick={ handleLogout }>Logout</Nav.Link>
                 : <Nav.Link as={ Link } to="/signup">Signup</Nav.Link>
             }     
             </Nav>
+            <Form className="d-flex vw-20">
+                <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-auto"
+                aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
             </Navbar.Collapse>
         </Container>
         </Navbar>
