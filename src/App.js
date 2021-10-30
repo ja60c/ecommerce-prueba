4aacda06c28a267b6b588d8ef7b0cce1193a5dca
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-10-30 13:56:02 -0500

    Quitando el home de ruta privada

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import ForgotPassword from './components/ForgotPassword';
 import UpdateProfile from './components/UpdateProfile';
 import Profile from './components/Profile';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <PrivateRoute exact path="/" component={ Products }/> 
           <PrivateRoute exact path="/profile" component={ Profile }/>
           <PrivateRoute exact path="/update-profile" component={ UpdateProfile }/>
           <Route path="/signup" component={ Signup }/>
           <Route path="/login" component={ Login }/>
           <Route path="/forgot-password" component={ ForgotPassword }/>
+          <Route path="/" component={ Products }/> 
         </Switch>
       </Router>
     </AuthProvider>
